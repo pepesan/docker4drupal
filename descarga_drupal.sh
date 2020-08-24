2a1a26ecb97fcbe32bf29bdf5781bf6737873bbd
pepesan <pepesan@gmail.com>
2020-08-24 12:40:15 +0200

    mejorada la creación de directorios de desarrollo

--- descarga_drupal.sh
+++ descarga_drupal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source .env
 composer create-project --no-interaction "drupal/recommended-project:$DRUPAL_VERSION" $DRUPAL_HOME  --stability stable --no-interaction
-mkdir "${DRUPAL_HOME}/web/sites/default/files"
+mkdir -p "${DRUPAL_HOME}/web/sites/default/files"
 chmod a+w "${DRUPAL_HOME}/web/sites/default/files"
-mkdir "${DRUPAL_HOME}/web/sites/default/files/translations"
+mkdir -p "${DRUPAL_HOME}/web/sites/default/files/translations"
 chmod a+w "${DRUPAL_HOME}/web/sites/default/files/translations"
-mkdir "${DRUPAL_HOME}/web/sites/default/files/tmp"
+mkdir -p "${DRUPAL_HOME}/web/sites/default/files/tmp"
 chmod a+w "${DRUPAL_HOME}/web/sites/default/files/tmp"
-mkdir "${DRUPAL_HOME}/web/sites/default/files/sync"
+mkdir -p "${DRUPAL_HOME}/web/sites/default/files/sync"
 chmod a+w "${DRUPAL_HOME}/web/sites/default/files/sync"
   #sudo cp drupal/web/sites/default/default.settings.php drupal/web/sites/default/settings.php
 cp settings.php "${DRUPAL_HOME}/web/sites/default/"
